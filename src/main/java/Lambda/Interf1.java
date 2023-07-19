745dd82fd2fd36982e1b8d9e1de238228939f159
pradeepkumar <pfm110820@gmail.com>
2023-07-19 14:50:06 +0530

    Lambda Expression in java 8 with SAM

--- src/main/java/Lambda/Interf1.java
+++ src/main/java/Lambda/Interf1.java
@@ -5,7 +5,7 @@ public interface Interf1 {
     default void testDefault(){
         System.out.println("Defalut method in java 8");
     }
-    static void testStatic(){
+    static void testStatic(){   //these methods cannot be overridden or changed in the implementation class
         System.out.println("Static method in java 8");
     }
 }
@@ -14,10 +14,11 @@ class Test{
     public static void main(String[] args) {
         Interf1  i=()-> System.out.println("Inetrface implemented with LAmbda");
         i.test(); //Normal interface method
-        i.testDefault(); //Default interface method
+        i.testDefault();//Default interface method
 
         Test t= new Test(); // calling static method with class name and interface name
         Interf1.testStatic();  //static method in interface
+        //these methods cannot be overridden or changed in the implementation class
     }
 }
 // output is
