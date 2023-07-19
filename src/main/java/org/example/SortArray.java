73918984d961cde34fa9ea21c9b7ddf1f3750b62
pradeepkumar <pfm110820@gmail.com>
2023-07-19 10:34:00 +0530

    modified name

--- src/main/java/org/example/SortArray.java
+++ src/main/java/org/example/SortArray.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Test {
+public class SortArray {
     public static void main(String[] args) {
         int[] arr={1,1,2,2,3,3,4,4,4,4,5,5,5,6};
         List<Integer> newArray= Arrays.stream(arr)
